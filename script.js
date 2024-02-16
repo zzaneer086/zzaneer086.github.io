e06bb01c74c44c1f9be16c396c3d922562162e5c
Zutzut <zainab4787@gmail.com>
2024-02-16 06:39:09 -0600

    everything lol

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function handleMouseOverProjects(element) {
+    // Change the background color or perform any other action when the mouse enters
+   document.getElementById("about").innerHTML = "not yet.";
+  
+}
+
+function handleMouseOutProjects(element) {
+    // Reset the background color or perform any other action when the mouse leaves
+    document.getElementById("about").innerHTML = "I'm currently a student aspiring to be a software developer.";
+}
+
+
+//document.getElementById("about").innerHTML = "Paragraph changed.";
